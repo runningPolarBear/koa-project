c91e4b03d5f8954aef48b45018260e05a3d5bd18
AlexHu <humeimanwj65365@touna.cn>
2018-08-23 10:08:15 +0800

    更新

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const Koa = require("koa");
+const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = "Hello World!";
-})
+// logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
 
-app.listen(3000)
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+app.use(async ctx => {
+    ctx.body = 'Hello World!';
+});
+console.log(app.env)
+app.listen(3000);
