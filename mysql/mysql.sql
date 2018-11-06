fafbeb3e88605f5868d4c8343e09304f2af1839a
AlexHu <humeimanwj65365@touna.cn>
2018-11-06 18:39:56 +0800

    增加sql

---
+++ mysql/mysql.sql
@@ -0,0 +1,15 @@
+create database myself;
+
+use myself;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL COMMENT '名称',
+  `password` varchar(20) COMMENT '密码',
+  `phone` int(11) NOT NULL DEFAULT '0' COMMENT '电话',
+  `created_time` DATETIME DEFAULT '1970-01-01 00:00:00' COMMENT '创建时间',
+  `modified_time` DATETIME DEFAULT '1970-01-01 00:00:00' COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `created_time` (`created_time`) USING BTREE,
+  KEY `modified_time` (`modified_time`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
